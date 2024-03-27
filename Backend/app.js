08aeafc9649a6f5e82d90b4ea4c3b690bf502868
gunika069 <gunikasharma0502@gmail.com>
2024-03-27 23:37:51 +0530

    Starting the project

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+require('dotenv').config()
+
+
+const PORT = process.env.PORT
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+
+
+const server = () =>{
+    app.listen(PORT,()=>{
+        console.log('listening to port:',PORT)
+    })
+
+}
+server()
